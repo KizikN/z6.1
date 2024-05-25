b3a114d8f55f0325a9d676a4591d3e581577f97f
KizikN <naemnik2.0@mail.ru>
2024-05-25 11:51:55 +0300

    Update TextBox.h

--- TextBox.h
+++ TextBox.h
@@ -18,5 +18,7 @@ public:
 	void Change_Color_R(unsigned int a) { T.R = a; }
 	void Change_Color_G(unsigned int a) { T.G = a; }
 	void Change_Color_B(unsigned int a) { T.B = a; }
+	bool Get_Readonly() { return Readonly; }
+	void Change_Readonly(bool a) { Readonly = a; }
 	friend std::ostream& operator <<(std::ostream& os, TextBox& V) { V.Print(); return os;}
 };
